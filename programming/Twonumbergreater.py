c0dd2c5c024e28213adadacf3ad6baca3a20f10c
Mohd Damish <danish.dmseo@gmail.com>
2024-05-24 09:35:03 +0530

    added new file comparison of two number

---
+++ programming/Twonumbergreater.py
@@ -0,0 +1,11 @@
+print("enter the first number")
+n1 = int(input())
+
+print("enter the second number")
+
+n2 = int(input())
+
+if n1>n2:
+    print("n1 is greater than n2")
+else:
+    print("n2 is greater")
