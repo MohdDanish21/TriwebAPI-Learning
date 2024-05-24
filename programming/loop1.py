7c10193815b0c41730a39cb0e1977693b2d3b288
Mohd Damish <danish.dmseo@gmail.com>
2024-05-24 16:51:37 +0530

    Make a program of loop to print numbers 1 to 10

--- programming/loop1.py
+++ programming/loop1.py
@@ -2,3 +2,6 @@ print(range(10))                 #to check range
 
 for i in range(10):              #to iterate loop
     print(i,"apple")
+
+for i in range(10):
+    print(i+1)                     #  print 1 to 10 
