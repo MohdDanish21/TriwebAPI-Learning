a924f4d76af19be45143fb46c98008c9ff0426e8
Mohd Damish <danish.dmseo@gmail.com>
2024-05-25 15:23:10 +0530

    added list using for and while loop

---
+++ programming/looplist.py
@@ -0,0 +1,28 @@
+# #how to print list without index
+
+# subjectMarks=[23,55,24,31,36]
+# for mark in subjectMarks:
+#     print(mark)
+
+
+
+#how to update values of list using index
+# subjectMarks=[23,55,24,31,36]
+# i=0
+# while i<5:
+#     subjectMarks[i] = subjectMarks[i] + 2
+#     i=i+1
+# print(subjectMarks)
+
+
+#how to add values in list
+subjectMarks=[23,55,24,31,36]
+subjectMarks.append(50)
+print(subjectMarks)
+
+
+#how to add values in list
+subjectMarks=[23,55,24,31,36]
+subjectMarks.pop()
+print(subjectMarks)          #pop remove from last
+                            #append add
