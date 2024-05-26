6afc2648a0ee78a9a40c046ce5f794dc92becd1b
Mohd Damish <danish.dmseo@gmail.com>
2024-05-26 18:28:43 +0530

    added two number using function

---
+++ programming/addtwonumberfunction.py
@@ -0,0 +1,32 @@
+# Add two number using function
+
+
+# def addNumber(num1,num2):
+#     sum = num1+num2
+#     print("sum=",sum)
+# addNumber(2,5)
+
+
+
+#2 add two number return the value from function
+
+# def addNumber(num1,num2):
+#     sum = num1+num2
+#     print("sum=",sum)
+#     return sum
+    
+# addNumber(2,5)
+
+
+#3 add two number add a number in variable
+
+def addNumber(num1,num2):
+    sum=num1+num2
+    return sum
+additionResult = addNumber(2,4)
+
+
+
+
+
+
---
+++ programming/function.py
@@ -0,0 +1,18 @@
+# def SomeFunctionName():
+#     print("india")
+# SomeFunctionName()
+
+
+
+
+ #2 with argument
+
+# def myFunctionName(num):
+#     print("the value of num is", num)
+# myFunctionName( 24)
+
+
+#3 with two argument
+def myFunctionName(num1,num2):
+    print("the value of num is", num1,num2)
+myFunctionName( 24,14)
