093955f5ae9eaf2281b4cdf7ea5d480b2ae2206a
Mohd Damish <danish.dmseo@gmail.com>
2024-05-27 10:33:25 +0530

    00ps concept in java

---
+++ oops/oops.java
@@ -0,0 +1,88 @@
+// OOPS moves around its four most important pillars. 
+// These are; 
+// Inheritance
+// Polymorphism
+// Abstraction
+// Encapsulation
+                                             // 1 (inheritance)---> 
+
+// Inheritance is a procedure to inherit the features from parent to child in the real world.
+//  Similarly, Inheritance in OOPS is a procedure by which one ClassClass acquires all the properties
+//  and behaviors of the parent class. 
+//  Inheritance ensures the reusability of the code.
+
+
+                                    // Types of Inheritance
+//                          There are a total of five types of Inheritance in OOPS.
+
+                                        // Single Inheritance
+                                        // Multilevel Inheritance
+                                        // Hierarchical Inheritance
+                                        // Multiple Inheritance
+                                        // Hybrid Inheritance
+
+
+
+//                                         Advantages of Inheritance
+                                            // Code reusability
+                                            // We can achieve Polymorphism using Inheritance. 
+
+
+
+
+
+//                                             Disadvantages of Inheritance
+
+
+// The child class and the parent class are tightly coupled. 
+// Any changes in the parent class equally affect all the child classes.
+
+
+
+                    // 2 (Polymorphism)
+
+
+// Poly means many, and morphism means forms. 
+// We know that Water also exists in multiple states, such as Solid, Liquid, and Gas. 
+// So Water shows Polymorphism
+
+
+                        // 3 (abstraction)
+
+// Abstraction is a property of hiding the internal implementation and highlighting the setup services beneficial to the user. 
+// For example, 
+// the smartphone user does not know the internal performance of the Smartphone and its workings; 
+// instead, they are interested in the services provided by the Smartphone. 
+
+//  we can achieve abstraction in two ways.
+
+// Using abstract ClassClass
+// using interfaces 
+
+
+
+
+// Advantages of abstraction 
+
+
+// Reduces complexity
+// Avoid code duplication 
+// Ease the burden of maintenance
+// Increase security and confidentiality 
+
+
+
+
+                                // 3 (Encapsulation)
+
+// Encapsulation is a mechanism to bundle the data and code acting on the data together as a single unit.
+//  there are two steps to achieve encapsulation in Java
+
+// declare the variable of a class as private
+// Provide a Public setter and getter method to modify and view the values of the variables.
+
+
+
+// Advantages of encapsulation 
+// Loosely coupled code 
+// Better access control and security 
