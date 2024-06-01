880a06bc7479b8c49f2d7c244c5b87d1960591ae
Mohd Damish <danish.dmseo@gmail.com>
2024-06-01 11:39:03 +0530

    Added a condition program to js file

---
+++ javascript/condition.js
@@ -0,0 +1,25 @@
+var x = 100;
+var y = 55;
+var z= 3;
+
+
+if(x>y){
+    console.log("x is greater than y")
+    //x z
+    if(x>z){
+        console.log("x is greater tan all")
+
+    }else{
+        console.log("z is greater than all")
+    }
+}
+else{
+    console.log("y is greater than x")
+    //y z
+    if(y>z){
+        console.log("y is greater than all")
+
+    }else{
+        console.log("z is greater than all")
+    }
+}
