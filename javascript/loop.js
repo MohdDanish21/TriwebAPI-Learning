6864230ccedf8d0a94af3459d78ba5d935e41dbe
Mohd Damish <danish.dmseo@gmail.com>
2024-06-02 15:51:33 +0530

    Added a program of  For loop

---
+++ javascript/loop.js
@@ -0,0 +1,15 @@
+for(let sn=1; sn<=5; sn++){
+    // console.log("india");
+    console.log(sn +"" ,"india");
+}
+
+// console.log(sn) here it will not work bcoz let and cost work in block statement
+
+
+//if we want to print outside the block statement....
+
+let sn1;
+for(sn1=1;sn1<5;sn1++){
+    console.log(sn1+"","india");
+}
+console.log(sn1);
