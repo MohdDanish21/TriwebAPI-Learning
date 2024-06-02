e1048a1d56de0632208ff8e3627b4e3f4c1e3926
Mohd Damish <danish.dmseo@gmail.com>
2024-06-02 12:16:07 +0530

    Added a file of different string and string literals

---
+++ javascript/literals.js
@@ -0,0 +1,19 @@
+console.log("literals");
+console.log("decimal",117);
+console.log("octal",0o77);
+console.log("hexadecimal",0x00111);
+console.log("binary",0b0011);
+console.log("Binary",0B0011);
+console.log("object literals")
+
+let obj = {name:"danish",mobile:"12345"}
+console.log(obj);
+console.log(obj.name);
+
+// string literals......>
+let name1='Danish';
+let s1='hi '+name1+'\how are you?';
+let s4 = `hi ${name1}
+how are you?`
+console.log("s1",s1);
+console.log("s4",s4);
