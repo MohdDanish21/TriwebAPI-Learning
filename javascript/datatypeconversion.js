38e0ba7192d8de02255c01713d327a5a2c10b2ed
Mohd Damish <danish.dmseo@gmail.com>
2024-06-02 11:47:08 +0530

    Added a file of data type conversion

---
+++ javascript/datatypeconversion.js
@@ -0,0 +1,25 @@
+let n  = 1;
+console.log(typeof(n));
+
+ n = "hi, how r you";
+ console.log(typeof(n));
+
+
+ let num1 = 4;
+ let num2 =5;
+ console.log(num1+num2);
+
+
+ let nu1 = '4';
+ let nu2 =5;
+ console.log(nu1+nu2); // string and perform Concatination
+
+
+ let n1 = '4';
+ let n2 =5;
+ console.log(parseInt(n1)+n2); // use of parseint changes string to number
+
+ 
+ let n3 = '4';
+ let n4 =5;
+ console.log(+n1+n2);   // use of + operator change string to number
