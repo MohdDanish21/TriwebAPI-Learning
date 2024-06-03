c2e1d097c3e2f5127b126e41736ad89df83a9abc
Mohd Damish <danish.dmseo@gmail.com>
2024-06-03 23:52:19 +0530

    Added  programs of hoist function and Recursion

---
+++ javascript/hoist function.js
@@ -0,0 +1,6 @@
+console.log(getNum3());
+function getNum3(){
+    return 3;
+}
+
+console.log(getNum3());
---
+++ javascript/recursion.js
@@ -0,0 +1,32 @@
+//When a function calling it self is called Recursion
+
+
+
+function printNumSeriesRecursion(num){
+    if(num==0){
+        return 0;
+    }
+    console.log(num);
+    num--
+    printNumSeriesRecursion(num);
+}
+printNumSeriesRecursion(5);
+
+
+console.log("........................")
+
+function printNumSeries(num){
+    for(let i =1; i<=num; i++){
+        console.log(i);
+    }
+}
+printNumSeries(5);
+
+console.log("...............")
+
+function printNumSeries1(num){
+    for(let i =num; i>0; i--){
+        console.log(i);
+    }
+}
+printNumSeries1(5);
