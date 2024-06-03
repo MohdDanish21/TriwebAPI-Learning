7b330748f3e80b73b0ef464384f6628ee2997634
Mohd Damish <danish.dmseo@gmail.com>
2024-06-03 11:08:28 +0530

    Added  a file of Break and continue  statement with single loop

---
+++ javascript/breakstatementwithsingle loop.js
@@ -0,0 +1,7 @@
+let n = 13;
+ for(let i =1; i<=n; i++){
+    console.log(i);
+    if(i==10){
+        break;
+    }
+ }
---
+++ javascript/continuestatementsingelloop.js
@@ -0,0 +1,8 @@
+let n= 13;
+for(let i=1; i<=n;i++){
+   
+    if(i==10){
+        continue;
+    }
+    console.log(i);
+}
