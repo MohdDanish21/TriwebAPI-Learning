8d440e8ad3619fc6b08094d4f937d79b86ee8bd6
Mohd Damish <danish.dmseo@gmail.com>
2024-06-03 22:53:10 +0530

    Added a Program of Function Expression

---
+++ javascript/functionexpression.js
@@ -0,0 +1,20 @@
+const square=function(num){
+    console.log(num*num);
+}                               // There are different way of function expression
+square(5);
+square(4);
+
+
+console.log("...............")
+
+function square1(n){
+    console.log(n*n)
+}
+square1(5)
+
+console.log("...............")
+
+const getCube =function cube(n){
+    console.log(n*n*n);
+}
+getCube(5)
