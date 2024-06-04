fc49dad706a45f939b28109999bd8ba0daf0ab9c
Mohd Damish <danish.dmseo@gmail.com>
2024-06-04 15:33:19 +0530

    Added functions of This and Number object methods

---
+++ javascript/number object method.js
@@ -0,0 +1,7 @@
+let x = 3/'r';
+console.log(x);
+if(!Number.isNaN(x)){
+    console.log('x is a number');
+}else{
+    console.log('x is NaN');
+}
---
+++ javascript/this.js
@@ -0,0 +1,10 @@
+function xyz(){
+    this.x =5;
+
+}
+
+let y =new xyz();
+let z =new xyz();
+console.log(y);
+z.x=8;
+console.log(z);
