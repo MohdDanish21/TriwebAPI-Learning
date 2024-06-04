23edd83095b43b1285af96270088deba6739b242
Mohd Damish <danish.dmseo@gmail.com>
2024-06-05 00:07:08 +0530

    Added a program of To lower and To upper string

---
+++ javascript/tolower and toupper.js
@@ -0,0 +1,17 @@
+const str= "Hi its a animal";
+const lowerStr=str.toLowerCase();
+console(lowerStr);
+
+
+const upperStr=str.toUpperCase();
+console(upperStr);
+
+const trimStr=str.trim();
+console.log(trimStr.length);
+
+
+//Multiline and embedded
+
+const str1= "how how are you.\its raining today";
+console.log(str1);
+
