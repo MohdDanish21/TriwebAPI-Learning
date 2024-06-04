4d01d80038c07154adea7a6e94204b71bcf7e411
Mohd Damish <danish.dmseo@gmail.com>
2024-06-04 15:44:52 +0530

    Added a function of Math object

---
+++ javascript/math object.js
@@ -0,0 +1,22 @@
+console.log(Map.PI);
+
+
+let max = Math.max(2,3,4,5);
+console.log('max',max);
+console.log('min',min);
+
+
+let ran = Math.random();
+console.log(ran);
+
+//one digit
+let ran1 = (Math.floor()*10,1);
+console.log("one digit",ran1);
+
+
+let num1 =355000000;
+let num2= num1.toExponential();
+console.log(num2);
+
+let num3= Number(1.34567).toFixed(2);
+console.log(num3);
