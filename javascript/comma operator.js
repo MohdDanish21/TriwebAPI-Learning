ec1a9ff77f24bf0474293a7424ff2e74a560884c
Mohd Damish <danish.dmseo@gmail.com>
2024-06-04 14:50:10 +0530

    Added functions of comma operator and unary operator

---
+++ javascript/comma operator.js
@@ -0,0 +1,9 @@
+let x = (3,4,5,6);
+console.log(x)// comma operator returned  last value
+
+for(let x=0,y=3;x<2; x++,y++){
+    console.log("x=",x);
+    console.log("y=",y)
+}
+
+// to handle multiple values in one loop
---
+++ javascript/unary operator.js
@@ -0,0 +1,18 @@
+// works on single operands
+
+let x = 4;
+let y = -x;
+console.log(y);
+
+
+let a = '2';
+// console.log(parseInt(a)+3);
+ console.log(+a+3);
+
+
+ // delete operator
+
+ let obj = {name:"hii", mobile:9800};
+ console.log(obj);
+ delete obj.mobile;
+ console.log(obj);
