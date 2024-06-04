f4492d5a281edea156b6bf01855b3ac6699e4594
Mohd Damish <danish.dmseo@gmail.com>
2024-06-04 18:28:08 +0530

    Added a program of text formatting

---
+++ javascript/text formatting.js
@@ -0,0 +1,18 @@
+const str1='India';
+console.log(str1);
+
+const str2=`india`;
+console.log(str2);
+
+
+const description ='"India" is my country';
+const description1 ="'India' is my country";
+const description2 ="'India' is my"+ '"country"';
+const description3 ="'India' is my\"country\"";
+const description4 ='"India" is my country\ n I love my country';
+
+console.log(description);
+console.log(description1);
+console.log(description2);
+console.log(description3);
+console.log(description4);
