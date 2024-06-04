1887dd90a7ff9733b5a476ed73afcebc6c1b755c
Mohd Damish <danish.dmseo@gmail.com>
2024-06-04 23:08:32 +0530

    Added programs of String object and Method of String Object

---
+++ javascript/method of string object.js
@@ -0,0 +1,17 @@
+const str = String("hi I am here");
+const firstChar=str.charAt(0);
+console.log("first character is",firstChar); //first character is H
+console.log("first character is",str[0]);// first character is H 
+
+
+const indexOfI=str.indexOf('I');
+console.log("Index of I is ",indexOfI);
+
+const indexOfi=str.indexOf('i');
+console.log("Index of i is ",indexOfi);
+
+const indexOfe=str.indexOf('e');
+console.log("Index of e is ",indexOfe);
+
+const lastIndexOfe=str.lastIndexOf('e');
+console.log("Index of e is ",lastIndexOfe);
---
+++ javascript/string object.js
@@ -0,0 +1,13 @@
+
+// string object ist method
+const strObj =new String("hi i am here");
+console.log(strObj);      // if you want object or wanna allocate space for object.
+
+
+//IInd method
+
+const strObj1 = String("hi i am here");
+console.log(strObj1);
+
+const str = "hi am here";
+console.log(str,typeof(str));
