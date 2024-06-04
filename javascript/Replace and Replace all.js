5c7f1105c220274eba320427542bfc2b90a89810
Mohd Damish <danish.dmseo@gmail.com>
2024-06-05 00:00:03 +0530

    Added a function of Replace and Replace all

---
+++ javascript/Replace and Replace all.js
@@ -0,0 +1,10 @@
+const str= "Hi its a animal";
+
+console.log(str);
+const str1=str.replace("animal","birds");
+console.log(str1);
+
+console.log("............................");
+const str2= "Hi its a animal, and its a big animal";
+const str3=str.replaceAll("animal","birds");
+console.log(str3);
