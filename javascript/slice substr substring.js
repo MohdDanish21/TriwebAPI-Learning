b131caedae6300752597d515e47004f95d733cfd
Mohd Damish <danish.dmseo@gmail.com>
2024-06-04 23:52:22 +0530

    Added a function of Slice subStr and substring

---
+++ javascript/slice substr substring.js
@@ -0,0 +1,13 @@
+// slice
+const str = "Hi I am here and its raining";
+const sliceStr=str.slice(0,2);
+console.log(sliceStr);
+
+//substring-end indexex
+
+const substringStr=str.substring(0,2);
+console.log(substringStr);
+
+//substr--num char
+const substrStr=str.substr(0,2);
+console.log(substrStr);
