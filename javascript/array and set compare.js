b716d767995b23b55e4ad559582a3b64ccfee143
Mohd Damish <danish.dmseo@gmail.com>
2024-06-05 14:17:43 +0530

    Added a function of Array Set compare

---
+++ javascript/array and set compare.js
@@ -0,0 +1,9 @@
+let mySet =new Set([2,3,4,5]);
+mySet.delete(3);
+console.log(mySet);
+
+
+let myArray=[2,3,4,5];
+// myArray.delete(2);
+myArray.splice(1,1);
+console.log(myArray);//myArray.delete is not a function
