0f6a6633f4226e5422ecf96a26e72c0e4aa950e2
Mohd Damish <danish.dmseo@gmail.com>
2024-06-05 10:23:01 +0530

    Added a function of Regular Expression

---
+++ javascript/Regular expression.js
@@ -0,0 +1,13 @@
+let str ="Hi I am Here. I an attending a workshop.";
+let RegExp=/\w/
+console.log(str.match(RegExp));
+
+console.log("............................");
+
+// let str1="Hi I am Here. I an attending a workshop.";
+// let regex=new RegExp('am');
+// let res= regex.test(str1);
+// console.log(res);
+
+
+// regular expression class used to test Occurence.
