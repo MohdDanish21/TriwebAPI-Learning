b6d68de3b10be5b8e52a739b7f3d19234079af97
Mohd Damish <danish.dmseo@gmail.com>
2024-06-05 13:51:08 +0530

    Added a function of set Method

---
+++ javascript/set.js
@@ -0,0 +1,16 @@
+let newSet= new Set()
+newSet.add(34);
+newSet.add(44);
+newSet.add(54);
+newSet.add(64);
+console.log(newSet);
+newSet.add(74);
+console.log(newSet);
+
+newSet.delete(74);
+console.log(newSet);
+
+if(newSet.has(64)){
+    console.log("Has method is working")
+}
+console.log(newSet);
