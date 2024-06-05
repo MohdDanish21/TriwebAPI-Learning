62226e33f92796a56e4d7a1ffce46c64486fb8ed
Mohd Damish <danish.dmseo@gmail.com>
2024-06-05 14:30:50 +0530

    Added a program working with object

---
+++ javascript/working with object.js
@@ -0,0 +1,12 @@
+let obj={name:"Danish",address:"india",mobile:989171}
+console.log(obj);
+
+
+console.log("..........................");
+let obj1 =new Object();
+obj1.name="Danish";
+obj1.mobile=89765;
+console.log(obj1);
+
+// The Variable which has been assigned as null contains no values. that is null
+// he Variable which has been declared but its value has not been assigned .that is undefined
