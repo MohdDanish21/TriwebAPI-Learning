ebaa50206561575a84f0f9c2a7ec0d75a68cdbe4
Mohd Damish <danish.dmseo@gmail.com>
2024-06-05 11:02:38 +0530

    Populated and Iterated of an array

---
+++ javascript/populate an array iterate.js
@@ -0,0 +1,22 @@
+let arr=[];
+
+arr[0]="India";
+arr[1]="us";
+console.log(arr);
+
+
+console.log(".....................");
+
+// length of an array
+let arr1 =[1,2,3,4,5,6,7,8];
+console.log("length of an array is",arr1.length);
+
+arr1.length=5;
+console.log(arr1);
+
+// iterate an array
+
+let arr2= ['A','B','C','D'];
+for(let i=0; i<arr2.length;i++){
+    console.log(arr2[i]);
+}
