0681cf4bc9cac0b4b8ea54ab613bfe60ff717b20
Mohd Damish <danish.dmseo@gmail.com>
2024-06-05 11:56:03 +0530

    Added a functions of foreach and map

---
+++ javascript/foreach and map using an array.js
@@ -0,0 +1,10 @@
+let arr =['you','hi','okay'];
+arr.forEach((element)=>{
+    console.log("I am with",element);
+})
+
+// map
+console.log("...................................");
+let arr1=[1,2,3,4,5,6];
+let arrSqr=arr1.map((element)=>element*element);
+console.log(arrSqr);
