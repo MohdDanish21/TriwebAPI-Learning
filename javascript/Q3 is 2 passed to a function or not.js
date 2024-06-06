f7d8ec93139fef272ec3f9e3a24e9c3dda716482
Mohd Damish <danish.dmseo@gmail.com>
2024-06-06 14:43:06 +0530

    Added a function to check a number is passed or not.

---
+++ javascript/Q3 is 2 passed to a function or not.js
@@ -0,0 +1,11 @@
+//How do you check that 2 is passed to a function or not?
+
+function isTwoPassed(){
+//return Array.from(arguments).indexOf(2)>=0;
+    if(Array.from(arguments).indexOf(2)>=0){
+        console.log("yes 2 is passed to this function");
+    }else{
+        console.log("no 2 is not passed to this function");
+    }
+}
+isTwoPassed(2);
