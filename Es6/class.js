f6ac09ae059344159f30b308e76d420a7c08ef15
Mohd Damish <danish.dmseo@gmail.com>
2024-06-07 18:05:24 +0530

    Added a program of class.

---
+++ Es6/class.js
@@ -0,0 +1,11 @@
+class Student{
+    constructor(sname,classId){
+        this.sname=sname;
+        this.classId=classId;
+    }
+}
+
+const Student1 =new Student("Azhar",6);
+const Student2 =new Student("Athar","7th");
+console.log(Student1);
+console.log(Student2);
