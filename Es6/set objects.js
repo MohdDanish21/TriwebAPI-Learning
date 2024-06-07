c844aa4108b24d9ab65980624e8aebccf01c09c0
Mohd Damish <danish.dmseo@gmail.com>
2024-06-07 17:57:44 +0530

    Added a program of st objects.

---
+++ Es6/set objects.js
@@ -0,0 +1,9 @@
+let newSet =new Set();
+newSet.add(1);
+newSet.add(2);
+newSet.add(3);
+newSet.add(4);
+newSet.add(4);
+console.log(newSet);// set does not  allow duplicate element
+
+// but in array case duplicate element allows
