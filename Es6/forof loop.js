1c931afa40664a518303363c0099e05126fc5e44
Mohd Damish <danish.dmseo@gmail.com>
2024-06-07 17:44:16 +0530

    Added a program of for of loop.

---
+++ Es6/forof loop.js
@@ -0,0 +1,21 @@
+let arr =[1,2,3,4,5];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    
+}
+
+
+
+console.log(".........................");
+// for of loop
+let arr1 =[1,2,3,4,5];
+
+for(num of arr1){
+    console.log(num);
+}
+
+// string case
+let str ="IAMHERE";
+for(let c of str){
+    console.log(c);
+}
