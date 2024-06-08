bde91ff7c5daea04907695d78b6f430e3350990b
Mohd Damish <danish.dmseo@gmail.com>
2024-06-08 11:52:33 +0530

    Added a function of Math

---
+++ Es6/math.js
@@ -0,0 +1,42 @@
+//math.trunc
+
+// console.log(math.trunc(5));
+// console.log(math.trunc(5.7));
+// console.log(math.trunc(5.1));
+// console.log(math.trunc(-5.9));
+
+// math .sign()
+
+// console.log(math.sign(-19));
+// console.log(math.sign(20));
+// console.log(math.sign(0));
+
+// math.cbrt()
+// console.log(math.cbrt(0));
+
+//math.log()
+// console.log(math.log(2));
+// console.log(math.log(10));
+
+//epsilon
+let x = number.EPSILON
+console.log(x);
+//min_safe_integer
+let y= Number.MIN_SAFE_INTEGER;
+console.log(y);
+
+//max_safe_integer
+let z= Number.MAX_SAFE_INTEGER;
+console.log(z);
+
+//number methods
+
+let a = 123;
+let b=9900719925478976
+console.log(number.isInteger(b));
+console.log("safe integer",number.isInteger(b));
+
+//global methods
+
+console.log("isFinite",isFinite(20000/0));false
+console.log("isNaN", isNaN(2/'e'));
