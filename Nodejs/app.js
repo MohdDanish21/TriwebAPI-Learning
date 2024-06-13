490654f5333b904bcc29d6d26758ce39ae1203c5
Mohd Damish <danish.dmseo@gmail.com>
2024-06-13 11:30:10 +0530

    Created a calculator using Router and Controller.

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,4 +1,5 @@
 const express =require('express');
+const calculatorRouter=require('./routers/calculator');
 
 const app=express();
 
@@ -8,22 +9,45 @@ app.get('/',(req,res)=>{
     res.send("I am response");
 });
 
-app.get('/add',(req,res)=>{
-    let n1=3;
-    let n2 =4;
-    let sum =n1+n2;
-    // res.send("Sum="+sum);
-    res.send(`sum=${sum}`);
-});
+// app.get('/add',(req,res)=>{
+//     let n1=3;
+//     let n2 =4;
+//     let sum =n1+n2;
+//     // res.send("Sum="+sum);
+//     res.send(`sum=${sum}`);
+// });
 
-app.post('/add',(req,res)=>{
-    // console.log(req.body);
-    // console.log(req.body.num1);    // if it gives body data then also give number one 
-    let n1 = req.body.num1;
-    let n2 = req.body.num2;
-    let sum=n1+n2;
-    // let sum=0;
-    res.send(`sum=${sum}`);
-});
+// app.post('/add',(req,res)=>{
+//     // console.log(req.body);
+//     // console.log(req.body.num1);    // if it gives body data then also give number one 
+//     let n1 = req.body.num1;
+//     let n2 = req.body.num2;
+//     let sum=n1+n2;
+//     // let sum=0;
+//     res.send(`sum=${sum}`);
+// });
+
+// app.post('/subtract',(req,res)=>{ 
+//     let n1 = req.body.num1;
+//     let n2 = req.body.num2;
+//     let diff=n1-n2;
+//     res.send(`Difference=${diff}`);
+// });
+
+// app.post('/multiply',(req,res)=>{ 
+//     let n1 = req.body.num1;
+//     let n2 = req.body.num2;
+//     let product=n1*n2;
+//     res.send(`product=${product}`);
+// });
+
+// app.post('/divide',(req,res)=>{ 
+//     let n1 = req.body.num1;
+//     let n2 = req.body.num2;
+//     let quotient=n1/n2;
+//     res.send(`Quotient=${quotient}`);
+// });
+
+app.use('/calculator',calculatorRouter);
 
 app.listen(3000);
---
+++ Nodejs/controllers/calculator.js
@@ -0,0 +1,27 @@
+module.exports.add =(req,res)=>{
+    let n1 =req.body.num1;
+    let n2 =req.body.num2;
+    let sum = n1+n2;
+    res.send(`sum=${sum}`);
+};
+
+module.exports.subtract =(req,res)=>{
+    let n1 =req.body.num1;
+    let n2 =req.body.num2;
+    let diff = n1-n2;
+    res.send(`difference=${diff}`);
+};
+
+module.exports.multiply =(req,res)=>{
+    let n1 =req.body.num1;
+    let n2 =req.body.num2;
+    let product = n1*n2;
+    res.send(`product=${product}`);
+};
+
+module.exports.divide =(req,res)=>{
+    let n1 =req.body.num1;
+    let n2 =req.body.num2;
+    let quotient = n1/n2;
+    res.send(`Quotient=${quotient}`);
+};
