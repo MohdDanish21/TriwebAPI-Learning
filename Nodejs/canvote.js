cc962e5d6aacfff62d0a064bffaac91dac41c219
Mohd Damish <danish.dmseo@gmail.com>
2024-06-16 00:12:48 +0530

    Added a program of eligibility criteria of vote.

---
+++ Nodejs/canvote.js
@@ -0,0 +1,20 @@
+let age =15;
+if(age>=18){
+    console.log("yes, you can vote");
+}
+else{
+    console.log("you can not vote");
+}
+
+
+// python program
+
+// print("enter your age to check that you are eligible for vote or not");
+
+// age =int(input())
+// print("your age is ",age)
+// if age >=18:
+//     print("you can vote")
+
+// else:
+//     print("you can not vote")
