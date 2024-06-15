182832fbcb77db388ba251163dd0415c8fe4ac9c
Mohd Damish <danish.dmseo@gmail.com>
2024-06-16 01:22:16 +0530

    Added a loop program.

---
+++ Nodejs/loop.js
@@ -0,0 +1,16 @@
+
+//This is python program
+
+// for i in Range(10);
+// print(i,"Azhar");
+
+
+
+// javscript loop program
+
+
+for (let i = 1; i<=10; i++) {
+    
+    console.log("i",i);
+    
+}
