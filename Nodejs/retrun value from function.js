d45b98297785f33d5ad5ae62d58e80b11ad61f3d
Mohd Damish <danish.dmseo@gmail.com>
2024-06-19 18:15:46 +0530

    Added a program of functiom with different method.

---
+++ Nodejs/retrun value from function.js
@@ -0,0 +1,23 @@
+// function addTwoNumber(num1,num2){
+//     sum =num1+num2;
+//     console.log("sum=",sum);
+// }
+// addTwoNumber(10,10);
+
+
+//2
+
+// function addTwoNumber(num1,num2){
+//     sum =num1+num2;
+//     return sum;
+// }
+// console.log(addTwoNumber(10,10));
+
+//3
+
+function addTwoNumber(num1,num2){
+    sum =num1+num2;
+    return sum;
+}
+additionResult=addTwoNumber(10,10);
+console.log(additionResult);
