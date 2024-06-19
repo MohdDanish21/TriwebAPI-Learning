46f2ce3ff62c758c8c689ecaf8caa79a42fb2535
Mohd Damish <danish.dmseo@gmail.com>
2024-06-19 16:57:14 +0530

    Added a program of reverse loop and print table.

---
+++ Nodejs/print table.js
@@ -0,0 +1,33 @@
+// start =1
+// end=10
+// console.log("start");
+// i=start;
+// while(i<=end){
+//     console.log(i*2);
+//     i++
+// }
+
+
+// table 
+// i=1
+// while(i<=10){
+//     console.log(i*3);
+//     i++
+// }
+
+
+
+// reverse 
+// i=10
+// while(i>=1){
+//     console.log(i);
+//     i--
+// }
+
+//reverse table
+
+i=10
+while(i>=1){
+    console.log(i*2);
+    i--
+}
