042e75cd6fbf7b0671fdb3fbef1a59b3a7c45690
Mohd Damish <danish.dmseo@gmail.com>
2024-06-19 15:30:44 +0530

    Added a progrma of while loop  with push function.

---
+++ Nodejs/loop list.js
@@ -0,0 +1,36 @@
+// subjectsMarks=[21,34,54,61,22]
+// let i =0;
+// while(i<5){
+//     subjectsMarks[1]=51
+//     console.log(i,"-",subjectsMarks[i]);
+    
+//     i++
+    
+// }
+// how to update value of list using index
+
+// subjectsMarks=[21,34,54,61,22]
+// let i =0;
+// while(i<5){
+//     // subjectsMarks[1]=51
+//     subjectsMarks[i]=subjectsMarks[i]+2;
+//     console.log(i,"-",subjectsMarks[i]);
+    
+//     i++
+    
+// }
+
+
+
+// want to add one more values
+
+subjectsMarks=[21,34,54,61,22]
+let i =0;
+while(i<6){
+        subjectsMarks.push(30);
+        
+        console.log(i,"-",subjectsMarks[i]);
+        
+    i++
+    
+}
