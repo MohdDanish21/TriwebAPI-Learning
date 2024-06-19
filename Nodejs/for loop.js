fa117385ff4950d7614dd2d8c98c96997ea69fee
Mohd Damish <danish.dmseo@gmail.com>
2024-06-19 11:41:37 +0530

    Added a program of loop with array list.

---
+++ Nodejs/for loop.js
@@ -0,0 +1,25 @@
+for(let i=1; i<=10; i=i+2){
+    console.log("print",i);
+}
+console.log("...................");
+
+// 2 python program
+// for i in Range(1,11,2):
+// print(i)
+
+// 3 print list[]
+// var arr = [];
+// for(var i = 0; i < 10; i++){
+//     arr[i] = 0;
+// }
+// console.log("print",arr);
+
+// let arr = [1, 2, 3, 4, 5];
+// arr = [];
+// console.log(arr); // []
+
+let arr = [1, 2, 3, 4, 5];
+for (let i = 0; i < 10; i++) {
+    console.log("print",arr);
+    
+}
