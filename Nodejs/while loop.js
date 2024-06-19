bf43855ee705a7cc36ef39ca92bfe5833abe086f
Mohd Damish <danish.dmseo@gmail.com>
2024-06-19 12:23:38 +0530

    Added a program of break and continue statement.

---
+++ Nodejs/while loop.js
@@ -0,0 +1,46 @@
+// let i =1;
+// while(i<=10){
+// console.log(i);
+// i++
+// }
+
+// this is condition based program.
+// let i =1;                      // here we are controlling printing not loop, loop is running continuously.
+// while(i<=10){
+//     if(i<5){
+//         console.log(i);
+//     }
+//     console.log("print if condition");
+// i++
+// }   
+
+
+// let i =1;                      // here we are controlling loop.
+// while(i<=10){
+//     if(i==5){
+        
+//         break;
+//     }
+//     console.log("print",i);
+// i++
+// }   
+
+
+// for loop
+
+
+for(let i =1; i<=10;i++){
+    if(i==5){
+        continue;
+       
+    }
+    console.log(i);
+}
+
+// for(let i =1; i<=10;i++){
+//     if(i==5){
+//         break;
+       
+//     }
+//     console.log(i);
+// }
