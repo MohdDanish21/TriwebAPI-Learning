08496067000db5cfec4e35c1ebcc7d6274a98c80
Mohd Damish <danish.dmseo@gmail.com>
2024-06-20 12:16:00 +0530

    Added concepts of oops and multithreadings.

---
+++ Nodejs/oops.js
@@ -0,0 +1,41 @@
+
+//List of OOPs Concepts in Java
+
+
+// 1.Class
+// Class is one of the OOPs in Java, which refers to a collection of objects. 
+// A class is a logical entity. It is one of the OOPs concepts in Java,
+//  with examples like the creation of a blueprint from which an individual object can be developed. 
+
+//2 Object
+// In object oriented programming, anything that has a state and behavior falls under the category of an object. 
+// It is one of the OOPs concepts in Java that can either be physical or logical.
+
+// 3.Inheritance
+// Inheritance refers to objects acquiring the behaviors and properties of a parent object. 
+// It is an OOPs concept in Java with reusable code. 
+// These Java OOPs concepts are useful for achieving runtime polymorphism. 
+// It is often easy to understand the OOPs concepts in Java with examples. 
+
+// 3.Polymorphism
+// Polymorphism is an OOPs concept in Java that revolves around performing one task in multiple ways. For instance, 
+// you might try to convince a customer in different ways. 
+// In Java object oriented programming, overloading and overriding methods are used to achieve polymorphism. 
+
+
+// 4.Abstraction
+// In object oriented programming, abstraction involves showing functionality while hiding internal details. 
+// Java OOPs concepts use abstract classes and interfaces to achieve abstraction.
+
+// 5.Encapsulation
+// Encapsulation is an OOPs concept in Java that involves binding code and data together in a single unit. 
+// If you want to understand these OOPs concepts in Java with examples, you should consider a Java class. 
+// Java Bean can be described as a fully encapsulated class because all the data members are private.
+
+
+// Multithreading in Java
+
+// Multithreading is a Java feature that allows concurrent execution of two or more parts of a program 
+// for maximum utilization of CPU. 
+// Each part of such program is called a thread. 
+// So, threads are light-weight processes within a process.
